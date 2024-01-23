0d814a54ada0fa5846b0d6192b4398b61b55e671
Sh1vamkushwah <103251988+Sh1vamkushwah@users.noreply.github.com>
2024-01-23 22:54:20 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs with given sum/count-pairs-with-given-sum.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            int ans = new Solution().getPairsCount(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int getPairsCount(int[] arr, int n, int target) {
+        
+        int count = 0 ;
+        
+         HashMap<Integer,Integer> hmap=new HashMap<>();
+         
+         for( int i = 0 ; i < n  ; i ++){
+             int diff = target - arr[i];
+             
+             if(hmap.containsKey(diff)){
+                 count = count + hmap.get(diff);
+                //  hmap.put(arr[i],hmap.get(arr[i])+1);
+             }
+             if(!hmap.containsKey(arr[i])){
+                 hmap.put(arr[i],1);
+             }else{
+                 hmap.put(arr[i],hmap.get(arr[i])+1);
+             } 
+             
+            //  if(hmap.containsKey(diff)){
+            //      count = count + hmap.get(diff);
+            //     //  hmap.put(arr[i],hmap.get(arr[i])+1);
+            //  }
+         }
+         
+         return count;
+    }
+}
